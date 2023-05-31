a2632bd1e53500a22f7981f2f00d45e463562891
Mo alotaibi <76629831+MBAalotaibi@users.noreply.github.com>
2023-05-31 19:02:46 +0100

    Add files via upload

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WindowsFormsControlLibrary1
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colFirstX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFirstY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSecondX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSecondY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colColor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.findIntersectionsButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Click += new System.EventHandler(this.splitContainer1_Panel1_Click);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
+            this.splitContainer1.Size = new System.Drawing.Size(1158, 577);
+            this.splitContainer1.SplitterDistance = 731;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.colorButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.removeButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.findIntersectionsButton, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(420, 573);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // colorButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.colorButton, 2);
+            this.colorButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.colorButton.Location = new System.Drawing.Point(3, 2);
+            this.colorButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(162, 53);
+            this.colorButton.TabIndex = 1;
+            this.colorButton.Text = "Color";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
+            // 
+            // removeButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.removeButton, 2);
+            this.removeButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.removeButton.Location = new System.Drawing.Point(171, 2);
+            this.removeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(162, 53);
+            this.removeButton.TabIndex = 2;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colFirstX,
+            this.colFirstY,
+            this.colSecondX,
+            this.colSecondY,
+            this.colColor});
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView1, 5);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 60);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 26;
+            this.dataGridView1.Size = new System.Drawing.Size(414, 223);
+            this.dataGridView1.TabIndex = 11;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_DefaultValuesNeeded);
+            // 
+            // colFirstX
+            // 
+            this.colFirstX.HeaderText = "FirstX";
+            this.colFirstX.MinimumWidth = 6;
+            this.colFirstX.Name = "colFirstX";
+            // 
+            // colFirstY
+            // 
+            this.colFirstY.HeaderText = "FirstY";
+            this.colFirstY.MinimumWidth = 6;
+            this.colFirstY.Name = "colFirstY";
+            // 
+            // colSecondX
+            // 
+            this.colSecondX.HeaderText = "SecondX";
+            this.colSecondX.MinimumWidth = 6;
+            this.colSecondX.Name = "colSecondX";
+            // 
+            // colSecondY
+            // 
+            this.colSecondY.HeaderText = "SecondY";
+            this.colSecondY.MinimumWidth = 6;
+            this.colSecondY.Name = "colSecondY";
+            // 
+            // colColor
+            // 
+            this.colColor.HeaderText = "Color";
+            this.colColor.MinimumWidth = 6;
+            this.colColor.Name = "colColor";
+            // 
+            // findIntersectionsButton
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.findIntersectionsButton, 2);
+            this.findIntersectionsButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.findIntersectionsButton.Location = new System.Drawing.Point(3, 288);
+            this.findIntersectionsButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.findIntersectionsButton.Name = "findIntersectionsButton";
+            this.findIntersectionsButton.Size = new System.Drawing.Size(162, 53);
+            this.findIntersectionsButton.TabIndex = 3;
+            this.findIntersectionsButton.Text = "Find Intersections";
+            this.findIntersectionsButton.UseVisualStyleBackColor = true;
+            this.findIntersectionsButton.Click += new System.EventHandler(this.findIntersectionsButton_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(1158, 577);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button findIntersectionsButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFirstX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFirstY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSecondX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSecondY;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colColor;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class UserControl1: UserControl
+    {
+        private List<Line> lines = new List<Line>();
+        HashSet<Point> intersections = new HashSet<Point>();
+        private bool drawIntersections = false;
+        private bool intersectionsDrawn = false;
+        private bool clickedOnce;
+        private Point clickedPoint1;
+        private Point clickedPoint2;
+        public UserControl1()
+        {
+            InitializeComponent();
+            Paint += splitContainer1_Panel1_Paint;
+            dataGridView1.Rows[0].Cells[4].Style.BackColor = Color.Black;
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 2);
+            foreach (var line in lines)
+            {
+                //set the color to draw the line with
+                if (line.Color != pen.Color)
+                    pen.Color = line.Color;
+                e.Graphics.DrawLine(pen, line.Point1, line.Point2);
+            }
+            //draw intersection if draw intersection was pressed
+            if (drawIntersections)
+            {
+                //reset pen color to default black color
+                pen.Color = Color.Black;
+                intersectionsDrawn = intersections.Count > 0;
+                foreach (Point intersection in intersections)
+                {
+                    e.Graphics.DrawEllipse(pen, intersection.X -  10, intersection.Y - 10, 20, 20);
+                }
+            }
+        }
+
+        //using intersection law of two lines from four points
+        private void findIntersectionsButton_Click(object sender, EventArgs e)
+        {
+            if (!drawIntersections)
+                drawIntersections = true;
+            //naive o(n^2) solution where we try all combinations to find intersections
+            for (int i=0; i<lines.Count; ++i)
+            {
+                //extract our lines points
+                Point point1 = lines.ElementAt(i).Point1;
+                Point point2 = lines.ElementAt(i).Point2;
+                //check against all others (that weren't checked) if there is an intersection
+                for (int j = i+ 1; j < lines.Count; j++)
+                {
+                    Point point3 = lines.ElementAt(j).Point1;
+                    Point point4 = lines.ElementAt(j).Point2;
+                    //check if parallel or coincident... denom is zero
+                    double denom = ((point1.X - point2.X) * (point3.Y - point4.Y)) - ((point1.Y - point2.Y) * (point3.X - point4.X));
+                    if (denom != 0)
+                    {
+                        //we have an intersection store it but first calc numerator1 and two
+                        double numeratorX = (
+                                          ((point1.X * point2.Y) - (point1.Y * point2.X)) * (point3.X - point4.X)) -
+                                          ((point1.X - point2.X) * ((point3.X * point4.Y) - (point3.Y * point4.X))
+                                          );
+                        double numeratorY = (
+                                          ((point1.X * point2.Y) - (point1.Y * point2.X)) * (point3.Y - point4.Y)) -
+                                          ((point1.Y - point2.Y) * ((point3.X * point4.Y) - (point3.Y * point4.X))
+                                          );
+                        Point intersection = new Point((int)(numeratorX / denom), (int)(numeratorY / denom));
+                        // Check if the intersection point lies within the line segments
+                        if (intersection.X < Math.Min(point1.X, point2.X) || intersection.X > Math.Max(point1.X, point2.X) ||
+                            intersection.X < Math.Min(point3.X, point4.X) || intersection.X > Math.Max(point3.X, point4.X) ||
+                            intersection.Y < Math.Min(point1.Y, point2.Y) || intersection.Y > Math.Max(point1.Y, point2.Y) ||
+                            intersection.Y < Math.Min(point3.Y, point4.Y) || intersection.Y > Math.Max(point3.Y, point4.Y))
+                        {
+                            // The intersection point is outside the line segments, no intersection
+                            continue;
+                        }
+                        intersections.Add(intersection);
+                    }
+                }
+            }
+            Refresh();
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("on color button clicked");
+            // Create a new instance of the ColorDialog
+            ColorDialog colorDialog = new ColorDialog();
+
+            // Show the color dialog and capture the result
+            DialogResult result = colorDialog.ShowDialog();
+
+            // Check if the user clicked the OK button in the dialog
+            if (result == DialogResult.OK)
+            {
+                // Retrieve the selected color
+                Color selectedColor = colorDialog.Color;
+                colorButton.ForeColor = selectedColor;
+
+                // Do something with the selected color
+                Console.WriteLine("Selected color: " + selectedColor);
+            }
+
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentRow.Index;
+            if (index < lines.Count)
+            {
+                lines.Remove(lines[index]);
+                dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+                //re calculate intersections
+                if (intersectionsDrawn)
+                {
+                    intersections = new HashSet<Point>();
+                    findIntersectionsButton_Click(sender, e);
+                }
+                Refresh();
+            }
+        }
+
+        private void splitContainer1_Panel1_Click(object sender, EventArgs e)
+        {
+            if (!clickedOnce)
+            {
+                clickedPoint1 = splitContainer1.Panel1.PointToClient(Cursor.Position);
+                System.Diagnostics.Debug.WriteLine("once");
+                clickedOnce = true;
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Added");
+                clickedPoint2 = splitContainer1.Panel1.PointToClient(Cursor.Position);
+                clickedOnce = false;
+                lines.Add(new Line(clickedPoint1, clickedPoint2, colorButton.ForeColor));
+
+                dataGridView1.Rows.Add(clickedPoint1.X, clickedPoint1.Y, clickedPoint2.X, clickedPoint2.Y);
+                dataGridView1.Rows[dataGridView1.Rows.Count - 2].Cells[4].Style.BackColor = colorButton.ForeColor;
+                Refresh();
+            }
+        }
+        private Color onColorCellClick()
+        {
+            ColorDialog colorDialog = new ColorDialog();
+
+            // Show the color dialog and capture the result
+            DialogResult result = colorDialog.ShowDialog();
+
+            // Check if the user clicked the OK button in the dialog
+            if (result == DialogResult.OK)
+            {
+                // Retrieve the selected color
+                Color selectedColor = colorDialog.Color;
+                return selectedColor;
+            }
+            return Color.Black;
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Cursor.Current = Cursors.IBeam;
+            if (dataGridView1.CurrentCell.ColumnIndex.Equals(4))
+            {
+                Color c = onColorCellClick();
+                dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[dataGridView1.CurrentCell.ColumnIndex].Style.BackColor = c;
+                if(dataGridView1.CurrentCell.RowIndex >= 0 && dataGridView1.CurrentCell.RowIndex < lines.Count)
+                    lines[dataGridView1.CurrentCell.RowIndex].Color = c;
+                Refresh();
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Done");
+            string x1 = Convert.ToString(dataGridView1.CurrentRow.Cells[0].Value);
+            string y1 = Convert.ToString(dataGridView1.CurrentRow.Cells[1].Value);
+            string x2 = Convert.ToString(dataGridView1.CurrentRow.Cells[2].Value);
+            string y2 = Convert.ToString(dataGridView1.CurrentRow.Cells[3].Value);
+            if (string.IsNullOrWhiteSpace(x1) || string.IsNullOrWhiteSpace(y1)
+                || string.IsNullOrWhiteSpace(x2) || string.IsNullOrWhiteSpace(y2))
+            {
+                return;
+            }
+            else if (!x1.All(Char.IsDigit) || !y1.All(Char.IsDigit)
+                || !x2.All(Char.IsDigit) || !y2.All(Char.IsDigit)) 
+            {
+                return;
+            }
+           
+            int firstX = Convert.ToInt32(x1);
+            int firstY = Convert.ToInt32(y1);
+            int secondX = Convert.ToInt32(x2);
+            int secondY = Convert.ToInt32(y2);
+
+            Point point1 = new Point(firstX, firstY);
+            Point point2 = new Point(secondX, secondY);
+            Color lineColor = dataGridView1.CurrentRow.Cells[4].Style.BackColor;
+            System.Diagnostics.Debug.WriteLine(lineColor);
+            if ((e.RowIndex + 1) > lines.Count)
+            {
+                lines.Add(new Line(point1, point2, lineColor));
+            }
+            else
+            {
+                lines[e.RowIndex] = new Line(point1, point2, lineColor);
+            }
+
+            Refresh();
+        }
+
+        private void dataGridView1_DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
+        {
+            e.Row.Cells["colColor"].Style.BackColor = Color.Black;
+            Invalidate();
+        }
+    }
+
+    public class Line
+    {
+        private Point point1;
+        private Point point2;
+        private Color color;
+
+        public Line(Point point1, Point point2)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            color = Color.Black;
+        }
+
+        public Line(Point point1, Point point2, Color color)
+        {
+            this.point1 = point1;
+            this.point2 = point2;
+            this.color = color;
+        }
+
+        public Color Color { get => color; set => color = value; }
+        public Point Point2 { get => point2; set => point2 = value; }
+        public Point Point1 { get => point1; set => point1 = value; }
+
+        public override string ToString()
+        {
+            return "First Point X: " + Point1.X + ", First Point Y: " + Point1.Y + 
+                ", Second Point X: " + Point2.X + ", Second Point Y: " + Point2.Y;
+        }
+    }
+}
